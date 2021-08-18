cfde2a117c50ece69f9dff89214018add822555b
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-08-18 20:40:06 +0100

    initial commit

--- components/Crypto/CryptoSectionThree.js
+++ components/Crypto/CryptoSectionThree.js
@@ -37,7 +37,30 @@ export default function CryptoSectionThree ({ tl }) {
           <div className='card-body'>
             <h2>Get started with the digitally economy</h2>
             <div className='row'>
-              <div className='col-md-4 flex-column align-items-center'></div>
+              <div className='col-md-4 flex-column align-items-center'>
+                <img src='' alt='' />
+                <p>Bitcoin</p>
+                <small>$46,567.00</small>
+                <span>+5.46%</span>
+              </div>
+              <div className='col-md-4 flex-column align-items-center'>
+                <img src='' alt='' />
+                <p>Ethereum</p>
+                <small>$3,239.87</small>
+                <span>+7.13%</span>
+              </div>
+              <div className='col-md-4 flex-column align-items-center'>
+                <img src='' alt='' />
+                <p>Dogecoin</p>
+                <small>$0.28</small>
+                <span>+6%</span>
+              </div>
+              <div className='col-md-4 flex-column align-items-center'>
+                <img src='' alt='' />
+                <p>Bitcoin Cash</p>
+                <small>$631.26</small>
+                <span>+4.49%</span>
+              </div>
             </div>
           </div>
         </div>
