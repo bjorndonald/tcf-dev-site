081f1cb77544dcd7d4eabc4288f0da1ce5623b69
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-08-18 20:49:18 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -20,9 +20,10 @@ module.exports = {
     loader: 'imgix',
     path: '/'
   },
-  rules: {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off'
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
   },
   webpack: config => {
     config.plugins.push(
