ca6d76637107e72ac1d3f0dda30639f5ee4a564a
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-10-04 13:42:58 +0100

    removed countdown

--- components/SignUp/signUpSectionOne.js
+++ components/SignUp/signUpSectionOne.js
@@ -8,7 +8,34 @@ function signUpSectionOne () {
       </div>
 
       <div className='page-options'>
-        <div className='page-option'></div>
+        <div className='page-option'>
+          <img src='/images/signup/landing.png' alt='' />
+          <div className='column'>
+            <h4>Funding</h4>
+            <a className='btn btn-black'>I want to get funded in 24 hours</a>
+          </div>
+        </div>
+        <div className='page-option'>
+          <img src='/images/signup/crypto.png' alt='' />
+          <div className='column'>
+            <h4>Crypto</h4>
+            <a className='btn btn-black'>I want to trade cryptocurrencies</a>
+          </div>
+        </div>
+        <div className='page-option'>
+          <img src='/images/signup/liquidity.png' alt='' />
+          <div className='column'>
+            <h4>Liquidity</h4>
+            <a className='btn btn-black'>I want to start a prop fund</a>
+          </div>
+        </div>
+        <div className='page-option'>
+          <img src='/images/signup/tools.png' alt='' />
+          <div className='column'>
+            <h4>Funding</h4>
+            <a className='btn btn-black'>I want to access trading software</a>
+          </div>
+        </div>
       </div>
     </div>
   )
