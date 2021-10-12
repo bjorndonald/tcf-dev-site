a191d90780612fcb3b0c8629ec33338ccfb03ccd
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-10-12 21:00:28 +0100

    sitemap

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -22,13 +22,29 @@ export const getServerSideProps = ({ res }) => {
       return `${baseUrl}/${staticPagePath}`
     })
 
+  var productPages = fs
+    .readdirSync(
+      {
+        development: 'pages/products',
+        production: './pages/products'
+      }[process.env.NODE_ENV]
+    )
+    .filter(staticPage => {
+      return !['_app.js', 'sitemap.js'].includes(staticPage)
+    })
+    .map(staticPagePath => {
+      return `${baseUrl}/products/${staticPagePath}`
+    })
+
+  staticPages.push(...productPages)
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${staticPages
           .map(url => {
             return `
               <url>
-                <loc>${url}</loc>
+                <loc>${url.replace('.js', '')}</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
                 <changefreq>monthly</changefreq>
                 <priority>1.0</priority>
