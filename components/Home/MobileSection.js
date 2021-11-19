cac1f4a4dc7f44f4e75dd6b7db96b1f2a8fc7895
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-11-19 21:45:10 +0100

    fix video issue

--- components/Home/MobileSection.js
+++ components/Home/MobileSection.js
@@ -70,10 +70,11 @@ function MobileSection (props) {
                     playsInline
                     className='image'
                     autoPlay='autoplay'
-                    muted={true}
+                    muted
                   >
                     <source src='/images/Landing.3gb' type='video/3gb' />
                     <source src='/images/Landing.webm' type='video/webm' />
+                    <source src='/images/Landing.mp4' type='video/mp4' />
                   </video>
                 </div>
                 <div className='box box2'></div>
