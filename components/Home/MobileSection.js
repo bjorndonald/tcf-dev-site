4e08d643dd1ddba62fcb59c0c99fb2a4d09faa34
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-12-01 12:54:12 +0100

    test

--- components/Home/MobileSection.js
+++ components/Home/MobileSection.js
@@ -73,7 +73,7 @@ function MobileSection (props) {
                     muted
                   >
                     {/* <source src='/images/Landing.3gb' type='video/3gb' />*/}
-                    <source src='/images/Landing.webm' type='video/webm' />
+                    {/* <source src='/images/Landing.webm' type='video/webm' /> */}
                     <source src='/images/Landing.mp4' type='video/mp4' />
                     <source src='/images/Landing.mov' />
                   </video>
