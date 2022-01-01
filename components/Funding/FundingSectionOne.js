53ff583f681a6eb77f5fd1ccaa2ef1f12ecd687b
Bjorn Donald <bjorndonaldb@yahoo.com>
2022-01-01 07:57:16 +0100

    changes

--- components/Funding/FundingSectionOne.js
+++ components/Funding/FundingSectionOne.js
@@ -3,6 +3,7 @@ import React from 'react'
 function FundingSectionOne () {
   return (
     <div className='fundingOne py-3 d-flex align-items-center justify-content-center'>
+      <div className='background'></div>
       <div className='container-fluid px-0 d-flex align-items-center justify-content-center'>
         <a
           href='https://fund.traderscentral.com/sign-up'
