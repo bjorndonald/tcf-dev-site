e6f6cca042b8e83f204d42f9007ac8f02805df19
Joshua Riverol <riveroljoshua1@gmail.com>
2022-02-28 19:28:47 -0600

    Setting back default funding hero section

--- components/Funding/FundingSectionOne.js
+++ components/Funding/FundingSectionOne.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
-function FundingSectionOne() {
+function FundingSectionOne () {
     return (
         <div className='fundingOne py-3 d-flex align-items-center justify-content-center'>
             <div className='background'></div>
-            <img className="backgroundImage" src="/images/funding/mol-banner.jpeg" alt=""/>
-            <a
-                href='https://fund.traderscentral.com/sign-up'
-                className='btn btn-black'
-            >
-                Start referring
-            </a>
+            <div className='container-fluid web px-0 d-flex align-items-center justify-content-center'>
+                {/* <h4>Get 30% off your first plan</h4> */}
+                <a
+                    href='https://fund.traderscentral.com/sign-up'
+                    className='btn btn-black'
+                >
+                    Get discount
+                </a>
+            </div>
+
+            <div className='container-fluid mobile px-0 d-flex align-items-center justify-content-center'>
+                <img src='/images/funding/new_year_banner_mobile.png' alt='' />
+                <h4>
+                    Get 30% off your <br />
+                    first plan
+                </h4>
+                <a
+                    href='https://fund.traderscentral.com/sign-up'
+                    className='btn btn-black'
+                >
+                    Get discount
+                </a>
+            </div>
         </div>
     )
 }
